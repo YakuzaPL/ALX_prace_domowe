8eb6d462e07ed94d86d23c7ea40b1339878bb931
Jakub <cedrik1410@gmail.com>
2020-03-31 21:44:52 +0200

    Zadanie_domowe_1.1

---
+++ Zad_dom1.2.py
@@ -0,0 +1,38 @@
+def user_data_input():
+    while True:
+        day = input("Proszę podać dzień w którym buty oddano do naprawy: ")
+        if day == "Poniedziałek" or day == "Wtorek" or day == "Śroida" or day == "Czwartek" or day == "Piątek" or day == "Sobota" or day == "Niedziela":
+            number_of_days = int(input("Proszę podać ile będzie trwała naprawa: : "))
+        else:
+            print("""Proszę wybrać:\n"
+                  "Poniedziałek"
+                  "Wtorek"
+                  "Środa"
+                  "Czwartek"
+                  "Piątek"
+                  "Sobota"
+                  "Niedziela""")
+            continue
+
+        return day, number_of_days
+
+
+def days_of_the_week_counter(day, numbers_of_days):
+    days_of_the_week = ("Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela")
+    day_number = days_of_the_week.index(day)
+    pick_up_day_num = (day_number + numbers_of_days) % 7
+    pick_up_day_name = days_of_the_week[pick_up_day_num]
+
+    return pick_up_day_name
+
+
+def main():
+    from_user = user_data_input()
+    days_calc = days_of_the_week_counter(from_user[0], from_user[1])
+    print(days_calc)
+
+    return
+
+
+main()
+
--- test.py
+++ test.py
@@ -1,1 +1,3 @@
-print("Moje prace domowe będą tu wprowadzane!")
+days_of_the_week = ("Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela")
+
+print(days_of_the_week.index("Poniedziałek"))
