bf12077e10970643b60081e0285a0783ebd03127
Jakub <cedrik1410@gmail.com>
2020-04-02 18:37:14 +0200

    Zadanie_domowe_1.5

---
+++ Zad_dom1.5.py
@@ -0,0 +1,42 @@
+from math import sqrt
+
+
+def side_info():
+    give_side_a = int(input("Proszę podać długość pierwszego boku: "))
+    give_side_b = int(input("Proszę podać długość drugiego boku: "))
+    give_side_c = int(input("Proszę podać długość trzeciego boku: "))
+
+    return give_side_a, give_side_b, give_side_c
+
+
+def triangle_checker(a, b, c):
+    sides = [a, b, c]
+    sides.sort()
+    a = sides[0]
+    b = sides[1]
+    c = sides[2]
+    if a + b > c:
+        calculator = tirangle_field_calc(a, b, c)
+    else:
+        print("Z takich boków nie da się zbudować trójkata.")
+        exit()
+    return calculator
+
+
+def tirangle_field_calc(side_a, side_b, side_c):
+    p = (side_a + side_b + side_c) / 2
+    field_value = sqrt(p * ((p - side_a) * (p - side_b) * (p - side_a)))
+
+    return field_value
+
+
+def main():
+    ask_user = side_info()
+    a = ask_user[0]
+    b = ask_user[1]
+    c = ask_user[2]
+    triangle = triangle_checker(a, b, c)
+    print(f"Pole powierzchni trójkąta o podanych bokach to: {triangle: .2f} cm2.")
+
+
+main()
--- test.py
+++ test.py
@@ -1,3 +1,16 @@
-days_of_the_week = ("Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela")
+def triangle_checker(a, b, c):
+    sides = [a, b, c]
+    sides.sort()
+    a = sides[0]
+    b = sides[1]
+    c = sides[2]
 
-print(days_of_the_week.index("Poniedziałek"))
+    if a + b > c:
+        print("OK")
+    else:
+        print("Z takich boków nie da się zbudować trójkata.")
+
+    return
+
+
+print(triangle_checker(5,2,4))
