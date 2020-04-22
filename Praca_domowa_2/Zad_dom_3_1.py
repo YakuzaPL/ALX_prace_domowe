331d35c892eb0e9082aedac1ad1f31e843bf8ad2
Jakub <cedrik1410@gmail.com>
2020-04-22 21:38:07 +0200

    Zadanie_domowe_3.1

--- Praca_domowa_2/Zad_dom_3_1.py
+++ Praca_domowa_2/Zad_dom_3_1.py
@@ -1,3 +1,6 @@
+import math
+
+
 def foot_to_metre(foot):
     return foot * 0.3048
 
@@ -9,4 +12,15 @@ def which_is_bigger(num_1, num_2):
         return num_2
     else:
         return "="
+
+
+def average_counter(num_1 = float, num_2 = float):
+    average = (num_1 + num_2) / 2
+    return average
+
+
+def area_of_the_circle(radius):
+    area = math.pi * (radius ** 2)
+    return area
+
 
--- Praca_domowa_2/Zad_dom_3_1_test.py
+++ Praca_domowa_2/Zad_dom_3_1_test.py
@@ -1,4 +1,4 @@
-from Praca_domowa_2.Zad_dom_3_1 import foot_to_metre, which_is_bigger
+from Praca_domowa_2.Zad_dom_3_1 import foot_to_metre, which_is_bigger, average_counter, area_of_the_circle
 
 
 def test_foot_to_metre():
@@ -15,3 +15,15 @@ def test_which_is_bigger():
     assert which_is_bigger(1, 1.5) == 1.5
     assert which_is_bigger(1, 1.000001) == 1.000001
 
+
+def test_average_counter():
+    assert average_counter(1, 1) == 1
+    assert average_counter(1, 2) == 1.5
+    assert average_counter(3, 9) == 6
+    assert average_counter(-2, 5) == 1.5
+
+
+def test_area_of_the_circle():
+    assert area_of_the_circle(2) == 12.566370614359172
+    assert area_of_the_circle(0) == 0.0
+    assert area_of_the_circle(23.4) == 1720.2104733996268
