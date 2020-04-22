5d60a0703985e06b60d75ad391fa0d57ef98cd38
Jakub <cedrik1410@gmail.com>
2020-04-22 21:12:12 +0200

    Zadanie_domowe_3.1

--- Praca_domowa_2/Zad_dom_3_1.py
+++ Praca_domowa_2/Zad_dom_3_1.py
@@ -2,3 +2,11 @@ def foot_to_metre(foot):
     return foot * 0.3048
 
 
+def which_is_bigger(num_1, num_2):
+    if num_1 > num_2:
+        return num_1
+    elif num_2 > num_1:
+        return num_2
+    else:
+        return "="
+
--- Praca_domowa_2/Zad_dom_3_1_test.py
+++ Praca_domowa_2/Zad_dom_3_1_test.py
@@ -1,4 +1,4 @@
-from Praca_domowa_2.Zad_dom_3_1 import foot_to_metre
+from Praca_domowa_2.Zad_dom_3_1 import foot_to_metre, which_is_bigger
 
 
 def test_foot_to_metre():
@@ -7,4 +7,11 @@ def test_foot_to_metre():
     assert foot_to_metre(-5) == -1.52400
     assert foot_to_metre(2500) == 762
     assert foot_to_metre(69.4) == 21.15312
+
+def test_which_is_bigger():
+    assert which_is_bigger(1, 2) == 2
+    assert which_is_bigger(2, 1) == 2
+    assert which_is_bigger(2, 2) == "="
+    assert which_is_bigger(1, 1.5) == 1.5
+    assert which_is_bigger(1, 1.000001) == 1.000001
 
