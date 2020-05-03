e7066365365f2b373c5cd5f4b4c35e5a75e7c9ce
Jakub <cedrik1410@gmail.com>
2020-05-03 21:11:12 +0200

    Zadanie_domowe_3.3

--- Praca_domowa_2/Zad_dom_3_3.py
+++ Praca_domowa_2/Zad_dom_3_3.py
@@ -26,12 +26,12 @@ def average_counter(list_of_numbers):
 
 
 def maximum_searcher(list_of_numbers):
-    a = list_of_numbers[0]
+    max_number = list_of_numbers[0]
     for number in list_of_numbers:
-        if number > a:
-            a = number
+        if number > max_number:
+            max_number = number
 
-    return a
+    return max_number
 
 
 def minmax_difference(list_of_numbers):
