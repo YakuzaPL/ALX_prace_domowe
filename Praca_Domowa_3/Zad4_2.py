2b66645ac4a26bf486cee92438a0180542c5786e
Jakub <cedrik1410@gmail.com>
2020-05-18 11:01:42 +0200

    Zadanie_domowe_4_2

--- Praca_Domowa_3/Zad4_2.py
+++ Praca_Domowa_3/Zad4_2.py
@@ -9,61 +9,39 @@ class Advertisement:
         self.vendro_mail = vendor_mail
 
     def make_adv(self):
-        advert = {"title": self.title,
-                  "Content": self.description,
-                  "Price": self.price,
-                  "vendro name": self.vendor_name,
-                  "Vendor tel": self.vendor_tel,
-                  "Vendor_mail": self.vendro_mail
-                  }
+        advert = [self.title, self.description, self.price, self.vendor_name, self.vendor_tel, self.vendro_mail]
         return advert
 
 
 class AdvertCollector():
     def __init__(self):
-        self.advert_colection = dict()
+        self.advert_colection = list()
 
-    def add_advertisment(self, advertisement: Advertisement, number):
-
-        for i in range(1, 4):
-            self.advert_colection[number] = advertisement.make_adv()
-
-        return self.advert_colection
+    def add_advertisment(self, advertisement: Advertisement):
+        """
+        Colecting advertisments from class Advertismen
+        :param advertisement: object
+        :return:
+        """
+        self.advert_colection.append(advertisement.make_adv())
 
     def print_advert(self):
-        for element in self.advert_colection.values():
-
-            print("-" * 35)
-            for cat, cont in element.items():
-                print(f"{cat}: {cont}")
+        for advert in self.advert_colection:
+            print(30 * "-")
+            for i in advert:
+                print(i)
 
     def __str__(self):
         return self.print_advert()
 
 
-# advert = AdvertCollector()
 advert1 = Advertisement("Sprzedam1", "sdafcniksdacniuansc", 659895, "Jakub", "560-987-744", "jz@mail.com")
 advert2 = Advertisement("Sprzedam2", "sdafcniksdacniuansc", 659895, "Zenek", "693-258-987", "zz@mail.com")
 advert3 = Advertisement("Sprzedam3", "sdafcniksdacniuansc", 659895, "Aga", "844-647-872", "az@mail.com")
 
-print(advert1)
+colector = AdvertCollector()
+colector.add_advertisment(advert1)
+colector.add_advertisment(advert2)
+colector.add_advertisment(advert3)
 
-colection = AdvertCollector()
-colection.add_advertisment(advert1, 1)
-# colection.adv_printer()
-colection.add_advertisment(advert2, 2)
-# colection.adv_printer()
-colection.add_advertisment(advert3, 3)
-colection.print_advert()
-
-print(30 * "*")
-a = colection.advert_colection.keys()
-b = colection.advert_colection
-
-a = 1
-for ad in b.values():
-    print("-"*30)
-    print(a)
-    a += 1
-    for cat, cont in ad.items():
-        print(f"{cat}: {cont}")
+print(colector.print_advert())
---
+++ test3.py
@@ -0,0 +1,8 @@
+test_list = [["Tytuł1", "bla bla bla bla bla", "Jakub", "506-585-098", "jz@mail.com"],
+             ["Tytuł2", "ra, la, la, la, la", "Michał", "506-555-258", "mj@mail.com"],
+             ["Tytuł3", "Oh my, oh my", "Aga", "513-888-258", "az@mail.com"]]
+
+for advert in test_list:
+    print(30*"-")
+    for i in advert:
+        print(i)
