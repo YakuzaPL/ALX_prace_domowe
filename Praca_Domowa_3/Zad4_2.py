dbfac45eeaf661b58805e3008947eb023d84c17b
Jakub <cedrik1410@gmail.com>
2020-05-19 21:08:19 +0200

    Zadanie_domowe_4_2_poprawione

--- Praca_Domowa_3/Zad4_2.py
+++ Praca_Domowa_3/Zad4_2.py
@@ -1,3 +1,6 @@
+
+
+
 class Advertisement:
 
     def __init__(self, title, description, price, vendor_name, vendor_tel, vendor_mail):
@@ -31,17 +34,30 @@ class AdvertCollector():
             for i in advert:
                 print(i)
 
+    def price_filterer(self, more_than: int = 0, less_than: int = 0):
+        for advert in self.advert_colection:
+            for content in advert:
+                if type(content) == int:
+                    if more_than < content < less_than:
+                        for advert_list in self.advert_colection:
+                            if content in advert_list:
+                                print(30 * "-")
+                                for data in advert_list:
+                                    print(data)
+
     def __str__(self):
         return self.print_advert()
 
 
-advert1 = Advertisement("Sprzedam1", "sdafcniksdacniuansc", 659895, "Jakub", "560-987-744", "jz@mail.com")
-advert2 = Advertisement("Sprzedam2", "sdafcniksdacniuansc", 659895, "Zenek", "693-258-987", "zz@mail.com")
-advert3 = Advertisement("Sprzedam3", "sdafcniksdacniuansc", 659895, "Aga", "844-647-872", "az@mail.com")
+advert1 = Advertisement("Sprzedam1", "sdafcniksdacniuansc", 450, "Jakub", "560-987-744", "jz@mail.com")
+advert2 = Advertisement("Sprzedam2", "sdafcniksdacniuansc", 4000, "Zenek", "693-258-987", "zz@mail.com")
+advert3 = Advertisement("Sprzedam3", "sdafcniksdacniuansc", 2800, "Aga", "844-647-872", "az@mail.com")
 
 colector = AdvertCollector()
 colector.add_advertisment(advert1)
 colector.add_advertisment(advert2)
 colector.add_advertisment(advert3)
 
-print(colector.print_advert())
+# print(colector.print_advert())
+
+colector.price_filterer(500, 3000)
--- test.py
+++ test.py
@@ -1,30 +1,35 @@
-advert = {1: {"title": "Tytuł1",
-              "Content": "bla, bla bla bla bala bla",
-              "vendro name": "Jakub",
-              "Vendor tel": "506-585-098",
-              "Vendor_mail": "jz@mail.com"},
-          2: {"title": "Tytuł2",
-              "Content": "ra, la, la, la, la",
-              "vendro name": "Michał",
-              "Vendor tel": "506-555-258",
-             "Vendor_mail": "mj@mail.com"},
-          3: {"title": "Tytuł3",
-                                                                                    "Content": "Oh my, oh my",
-                                                                                    "vendro name": "Aga",
-                                                                                    "Vendor tel": "513-888-258",
-                                                                                    "Vendor_mail": "az@mail.com"
-                                                                                    },
-          4: {"title": "Tytuł4",
-                                                                                           "Content": "Oh my, oh my",
-                                                                                           "vendro name": "Aga",
-                                                                                           "Vendor tel": "513-888-258",
-                                                                                           "Vendor_mail": "az@mail.com"
-                                                                                           }}
-
-for ad in advert.values():
-    print("-" * 35)
-    for cat, cont in ad.items():
-        print(f"{cat}: {cont}")
 
 
-print(advert.keys())
+advertisment = [["Ogłoszenie 1", "Treść ogłoszenia 1", 3520, "Imię sprzedającego 1", "Mail 1", "telefon 1"],
+                ["Ogłoszenie 2", "Treść ogłoszenia 2", 480, "Imię sprzedającego 2", "Mail 2", "telefon 2"],
+                ["Ogłoszenie 3", "Treść ogłoszenia 3", 56896, "Imię sprzedającego 3", "Mail 3", "telefon 3"],
+                ["Ogłoszenie 4", "Treść ogłoszenia 4", 1200, "Imię sprzedającego 4", "Mail 4", "telefon 4"],
+                ["Ogłoszenie 5", "Treść ogłoszenia 5", 98, "Imię sprzedającego 5", "Mail 5", "telefon 5"],
+                ["Ogłoszenie 6", "Treść ogłoszenia 6", 1587415, "Imię sprzedającego 6", "Mail 6", "telefon 6"]]
+
+
+def advertismen_searcher(list_of_adverts):
+    for advert in list_of_adverts:
+        print(30 * "-")
+        for content in advert:
+            print(content)
+
+
+def price_filterer(list_of_adverts, more_than: int = 0, less_than: int = 0):
+    for advert in list_of_adverts:
+        for content in advert:
+            if type(content) == int:
+                if more_than < content < less_than:
+                    for advert_list in list_of_adverts:
+                        if content in advert_list:
+                            print(30 * "-")
+                            for data in advert_list:
+                                print(data)
+
+
+print(price_filterer(advertisment, 150, 2500))
+
+# numbers = [1, 2, 3, 4, 5, 6]
+#
+# list_filter = filter(lambda number: number < 4, numbers)
+# filtered_list = list(list_filter)
